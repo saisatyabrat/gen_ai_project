68fecf4f5c8badbf6ddc6add0d66c38f1b1f35ef
Sai Satyabrat Biswal <121072358+saisatyabrat@users.noreply.github.com>
2024-04-17 15:35:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,35 @@ genai.configure(api_key=OPENAI_API_KEY)
 st.title("😍💕Well-come to my code debuger app😍💕")
 
 
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
 promt = st.text_input("Enter your python code")
 
